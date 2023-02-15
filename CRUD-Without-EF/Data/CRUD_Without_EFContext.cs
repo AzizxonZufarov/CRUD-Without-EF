5414bcbe670016c3a220228ad8e3d6e6b47c5036
AzizxonZufarov <91425039+AzizxonZufarov@users.noreply.github.com>
2023-02-15 08:38:40 +0500

    first commit

---
+++ CRUD-Without-EF/Data/CRUD_Without_EFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUD_Without_EF.Models;
+
+namespace CRUD_Without_EF.Data
+{
+    public class CRUD_Without_EFContext : DbContext
+    {
+        public CRUD_Without_EFContext (DbContextOptions<CRUD_Without_EFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUD_Without_EF.Models.BookViewModel> BookViewModel { get; set; }
+    }
+}
---
+++ CRUD-Without-EF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_Without_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
